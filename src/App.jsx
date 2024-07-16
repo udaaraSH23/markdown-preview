516b10a36c5527d8de60404d800ad5f77720aa40
unknown <udarasenarath875@gmail.com>
2024-07-16 19:47:54 +0530

    Passed the test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,40 @@ import React, { useState, useRef } from "react";
 import ReactMarkdown from "react-markdown";
 
 function App() {
-  const [markdown, setMarkdown] = useState("# Enter your Markdown here");
+  const [markdown, setMarkdown] = useState(`# Welcome to my React Markdown Previewer!
+
+    ## This is a sub-heading...
+    ### And here's some other cool stuff:
+    
+    Heres some code, \`<div></div>\`, between 2 backticks.
+    
+    \`\`\`
+    // this is multi-line code:
+    
+    function anotherExample(firstLine, lastLine) {
+      if (firstLine == '\\\`\`\`' && lastLine == '\\\`\`\`') {
+        return multiLineCode;
+      }
+    }
+    \`\`\`
+    
+    You can also make text **bold**.
+    
+    
+    There's also [links](https://www.freecodecamp.org), and
+    > Block Quotes!
+    
+    - And of course there are lists.
+      - Some are bulleted.
+         - With different indentation levels.
+            - That look like this.
+    
+    
+    1. And there are numbered lists too.
+    1. Use just 1s if you want!
+    1. And last but not least, let's not forget embedded images:
+    
+    ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`);
 
   const textareaRef = useRef(null);
 
@@ -38,11 +71,11 @@ function App() {
             onChange={handlefunc}
           ></textarea>
         </div>
-        <div className="col m-2 bg-success-subtle">
+        <div className="col m-2 bg-success-subtle" >
           <div className="row bg-success">
             <p className="h1 m-3 text-dark ">Preview</p>
           </div>
-          <div className=" overflow-auto mt-2 p-2 ">
+          <div className=" overflow-auto mt-2 p-2 " id="preview">
             <ReactMarkdown>{markdown}</ReactMarkdown>
           </div>
         </div>
